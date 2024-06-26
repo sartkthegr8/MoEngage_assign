017f71d211b3080c458279c8d0b059bbea4da6b0
sarthak <sartkthegr8@gmail.com>
2024-06-26 13:21:56 +0530

    done 2

--- frontend/src/helper/auth.js
+++
@@ -1,7 +0,0 @@
-import { sessionService } from "redux-react-session";
-
-export function logoutUser(navigate) {
-    sessionService.deleteSession();
-    sessionService.deleteUser();
-    navigate && navigate();
-  }
--- frontend/src/store/reducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import userAuth from "store/modules/usersAuth/userAuth.reducer";
-import { sessionReducer } from "redux-react-session";
-
-export default combineReducers({
-  session: sessionReducer,
-  userAuth,
-});
